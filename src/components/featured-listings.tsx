a8c20b41ad7d8e0a0bcc792f230ba18dce1b5e47
paulinasprojects <virtualpaula93@gmail.com>
2024-12-27 15:31:07 +0200

    added  responsive featured listings section

--- src/components/featured-listings.tsx
+++ src/components/featured-listings.tsx
@@ -1,6 +1,38 @@
+import { featuredListingsData } from "@/lib/data";
+import { MdLocationOn } from "react-icons/md";
+import "@/styles/featured-listings.scss";
+
 const FeaturedListings = () => {
   return (
-    <div>FeaturedListings</div>
+    <div className="featured-listings-container">
+      <div className="featured-title-container">
+        <span className="featured-listing-title">Featured Listings</span>
+        <span className="featured-listing-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br /> tempor incididunt ut labore et dolore magna aliqua.</span>
+      </div>
+      <div className="featured-listings-items-container">
+        {featuredListingsData.map((item) => (
+          <div>
+            <div className="relative | relative-container">
+              <img src={item.image} alt="" className="featured-listing-image" />
+              <div className="absolute | absolute-container">
+                <span className="featured-listing-price">${item.price},-</span>
+              </div>
+              <div className="featured-item-title-container">
+                <span className="featured-item-title">{item.title}</span>
+              </div>
+              <div className="featured-item-location">
+                <MdLocationOn className="location-icon" />
+                <span className="featured-item-location-title">{item.location}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="see-more-container">
+        <span className="see-more-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br className="featured-br" /> eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+        <button className="seem-more-button">See more</button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/lib/data.ts
@@ -0,0 +1,35 @@
+import imageone from "/featured-image-1.png";
+import imagetwo from "/featured-image-2.png";
+import imagethree from "/featured-image-3.png";
+import imagetfour from "/featured-image-4.png";
+
+export const featuredListingsData = [
+  {
+    id: 1,
+    image: imageone,
+    price: 2800,
+    title: "Ataz Standard House",
+    location: "Pluto Street, Indonesia",
+  },
+  {
+    id: 2,
+    image: imagetwo,
+    price: 4800,
+    title: "Qiri Minimalist House",
+    location: "Jupiter Street, Indonesia",
+  },
+  {
+    id: 3,
+    image: imagethree,
+    price: 6800,
+    title: "Qanan Luxury House",
+    location: "Michigan Street, USA",
+  },
+  {
+    id: 4,
+    image: imagetfour,
+    price: 8800,
+    title: "Tingah Elegant House",
+    location: "Halsted Street, USA",
+  },
+];
