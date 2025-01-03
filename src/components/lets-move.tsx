e7b0a968652ffad597d3f61ebc36329dd4b10f08
paulinasprojects <virtualpaula93@gmail.com>
2025-01-03 10:31:33 +0200

    added responsive Let's move section

---
+++ src/components/lets-move.tsx
@@ -0,0 +1,33 @@
+import "@/styles/lets-move.scss";
+import moveimage from '/move-image.png';
+import galleryimageone from '/move-gallery-image.png';
+import galleryimagetwo from '/move-gallery-image-two.png'
+import galleryimagethree from '/move-gallery-image-three.png'
+
+const LetsMove = () => {
+  return (
+    <div className="lets-move-container">
+      <div className="flex | lets-move-content-container">
+        <div>
+          <img src={moveimage} alt="" className="move-image" />
+        </div>
+        <div className="flex-col">
+          <span className="lets-move-title">Let's Move to Your Dream Family Home</span>
+          <span className="lets-move-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua.</span>
+        {/* Button Container */}
+        <div className="lets-move-button-container">
+          <button className="lets-move-button">Get Quote Now</button>
+        </div>
+        {/* Gallery Container */}
+        <div className="lets-move-gallery-container">
+          <img src={galleryimageone} alt=""  className="lets-move-gallery-image"/>
+          <img src={galleryimagetwo}alt=""  className="lets-move-gallery-image"/>
+          <img src={galleryimagethree}alt=""  className="lets-move-gallery-image"/>
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LetsMove
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,6 +1,7 @@
 import BuyPropertySteps from "@/components/buy-property-steps"
 import FeaturedListings from "@/components/featured-listings"
 import Hero from "@/components/hero"
+import LetsMove from "@/components/lets-move"
 import OurVision from "@/components/our-vision"
 import WhatOurCustomersSaid from "@/components/what-our-customers-said"
 import WhyChooseUs from "@/components/why-choose-us"
@@ -14,6 +15,7 @@ const HomePage = () => {
       <BuyPropertySteps/>
       <WhyChooseUs/>
       <WhatOurCustomersSaid/>
+      <LetsMove/>
     </>
   )
 }
