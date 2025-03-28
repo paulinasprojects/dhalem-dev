b2df00a869343489a48a700dc3f1bcd929b1b55f
paulinasprojects <virtualpaula93@gmail.com>
2025-03-28 16:09:52 +0200

    added animation to our team page

--- src/components/our-team-gallery.tsx
+++ src/components/our-team-gallery.tsx
@@ -1,19 +1,31 @@
+import { motion } from "framer-motion";
+import { containerVariants, itemVariants } from "@/lib/constants";
 import { ourTeamList } from "@/lib/data";
 import "@/styles/our-team-gallery.scss";
 
 const OurTeamGallery = () => {
   return (
-    <div className="our-team-gallery-main-container">
+    <motion.div 
+      className="our-team-gallery-main-container"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{once: true, amount: 0.2}}
+      variants={containerVariants}
+    >
       <div className="our-team-gallery-content-container">
         {ourTeamList.map((member) => (
-          <div key={member.id} className="our-team-gallery-content">
+          <motion.div 
+            key={member.id} 
+            className="our-team-gallery-content"
+            variants={itemVariants}
+          >
             <img src={member.image} className="our-team-gallery-image" />
             <span className="our-team-gallery-name">{member.name}</span>
             <span className="our-team-gallery-work-position">{member.workPosition}</span>
-          </div>
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
